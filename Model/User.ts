565dec7f4b511badc15c15e441a79a015378a040
unknown <hirunahansindu123@gmail.com>
2026-03-04 17:50:32 +0530

    add user file

--- Model/User.ts
+++ Model/User.ts
@@ -1,38 +1,36 @@
 import mongoose, { Document } from "mongoose";
 
-export interface ITeamMember extends Document {
+export interface IUser extends Document {
     name: string;
-    role: string;
-    description: string;
     email: string;
+    password: string;
+    role: string;
 }
 
-const teamMembersSchema = new mongoose.Schema<ITeamMember>({
-    name : {
+const userSchema = new mongoose.Schema<IUser>({
+    name: {
         type: String,
         required: true,
         trim: true
     },
-
-    role: {
-        type: String,
-        required: true,
-        trim: true
-    },
-
-    description: {
-        type: String,
-        required: true,
-        trim: true
-    },
-
     email: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    role: {
         type: String,
         required: true,
         trim: true
     }
 });
 
-const TeamMembers = mongoose.model<ITeamMember>("teamMembers", teamMembersSchema);
+const User = mongoose.model<IUser>("User", userSchema);
 
-export default TeamMembers;
+export default User;
---
+++ Routes/UserRoutes.ts
@@ -0,0 +1,84 @@
+import { Router, Request, Response } from "express";
+import User from "../Model/User";
+
+const router: Router = Router()
+
+// @route Post /api/users
+router.post("/", async (req: Request, res: Response):Promise <void> => {
+    try{
+        const {
+            name,
+            email,
+            password,
+            role
+        } = req.body
+        const user = new User({
+            name,
+            email,
+            password,
+            role
+        })
+        const addUser = await user.save()
+        res.status(201).json(addUser);
+    } catch (error) {
+        res.status(500).json({ error: "Error creating user" });
+        return;
+    }
+});
+
+// @route Get /api/users
+router.get("/", async (req: Request, res: Response): Promise<void> => {
+    try{
+        const users = await User.find()
+        res.status(200).json(users)
+    } catch (error) {
+        res.status(500).json({ error: "Error fetching users" });
+        return;
+    }
+});
+
+// @route Put /api/users/:id
+router.put("/:id", async (req: Request, res: Response): Promise<void> => {
+    try{
+        const {
+            name,
+            email,
+            password,
+            role
+        } = req.body
+        const user = await User.findByIdAndUpdate(req.params.id)
+        if(user){
+            user.name = name || user.name;
+            user.email = email || user.email;
+            user.password = password || user.password;
+            user.role = role || user.role;
+
+            const updateUser = await user.save();
+            res.status(200).json(updateUser);
+        } else {
+            res.status(404).json({ error: "User not found" });
+            return;
+        }
+        
+    } catch (error) {
+        res.status(500).json({ error: "Error updating user" });
+        return;
+    }
+});
+
+// @route Delete /api/users/:id
+router.delete("/:id", async (req: Request, res: Response): Promise<void> => {
+    try{
+        const user = await User.findById(req.params.id)
+        if(user){
+            await user.deleteOne();
+            res.status(200).json({ message: "User deleted successfully" });
+        } else {
+            res.status(404).json({ error: "User not found" });
+            return;
+        }
+    } catch (error) {
+        res.status(500).json({ error: "Error deleting user" });
+        return;
+    }
+});
